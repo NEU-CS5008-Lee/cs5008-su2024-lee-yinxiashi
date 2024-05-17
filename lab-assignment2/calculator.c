39e5e83e3f16301e10cb356fb6b754be030c99cc
Yinxia Shi <yancy.syx@gmail.com>
2024-05-16 20:41:22 -0700

    Submit lab assignment 2

--- lab-assignment2/calculator.c
+++ lab-assignment2/calculator.c
@@ -1,18 +1,48 @@
-//enter your name here
-//enter your email here
-#include<stdio.h>
-int main()
-{
-    int a,b,c, choice;
+// Name:  Yinxia Shi
+// Email: shi.yinx@northeastern.edu
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, choice;
     
     printf("Enter your choice\n");
-    printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
-    scanf("%d",&choice);
+    printf(" 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
+    scanf("%d", &choice);
     
-    printf("Enter a and b values");
-    scanf("%d %d", &a, &b);
+    printf("Enter a and b values ");
+    scanf("%d%d", &a, &b);
     
     // Add your code here using switch
+    switch (choice) {
+    case 1:
+        printf("Addition\n");
+        c = a + b;
+        printf("Sum=%d\n", c);
+        break;
+    case 2:
+        printf("Subtraction\n");
+        c = a - b;
+        printf("Difference=%d\n", c);
+        break;
+    case 3:
+        printf("Multiplication\n");
+        c = a * b;
+        printf("Product=%d\n", c);
+        break;
+    case 4:
+        printf("Division\n");
+        if (b == 0) {
+            printf("divide by zero error\n");
+            break;
+        }
+        c = a % b;
+        printf("Remainder=%d\n", c);
+        break;
+    default:
+        printf("Invalid option");
+        break;
+    }
            
     return 0;
 }
--- lab-assignment2/employee.c
+++ lab-assignment2/employee.c
@@ -1,34 +1,60 @@
-/*C Program to read and print the n employee details using structure and dynamic memory allocation*/
+/**
+ * C Program to read and print the n employee details
+ * using structure and dynamic memory allocation
+ * 
+ * Name:  Yinxia Shi
+ * Email: shi.yinx@northeastern.edu
+ */
 #include <stdio.h>
 #include <stdlib.h>
-/*structure to hold the details of an employee*/
-typedef struct employee
-{
+
+/**
+ * structure to hold the details of an employee
+ */
+typedef struct employee {
     int empId;
     char Name[100];
     char Designation[100];
     char Dept[10];
 
 }employee_t;
+
 /* Read the employee details using scanf*/
-void readData(int n, employee_t* s)
-{
+void readData(int n, employee_t* s) {
     // Add your code here
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("\nEnter the employeeID, Name, Designation and Department of Employee %d:\n", i + 1);
+        scanf("%d", &(s->empId));
+        scanf("%s", s->Name);
+        scanf("%s", s->Designation);
+        scanf("%s", s->Dept);
+        s++;
+    }
 }
+
 /* Function to print the employee details*/
-void display(int n, employee_t * s)
-{
+void display(int n, employee_t* s) {
     // Add your code here
-    
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("\nThe details of the Employee %d:\n", i + 1);
+        printf("empId: %d\n", s->empId);
+        printf("Name: %s\n", s->Name);
+        printf("Designation: %s\n", s->Designation);
+        printf("Dept: %s\n", s->Dept);
+        s++;
+    }
 }
 
 /*----------Main Program--------*/
-int main()
-
-{
+int main() {
     // Main Function to print the employee details
     // Add your code here
-    
-
+    int n;
+    printf("\nEnter the number of employees: ");
+    scanf("%d", &n);
+    employee_t* s = (employee_t*)malloc(n * sizeof(employee_t));
+    readData(n, s);
+    display(n, s);
 }
-
