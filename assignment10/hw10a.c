dbe2a67a1861166682996f450d24ef31dd77a16a
Yinxia Shi <yancy.syx@gmail.com>
2024-07-14 16:28:57 -0700

    Update hw 10a.

--- assignment10/hw10a.c
+++ assignment10/hw10a.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Yinxia Shi
+// email: shi.yinx@northeastern.edu
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,7 +26,7 @@ bool setNotEmpty(bool s[], int size) {
   bool result;
 
   result = false;
-  for (i=1; i<size; i++) {
+  for (i = 1; i < size; i++) {
     result = result || s[i];
   }
 
@@ -41,14 +41,13 @@ int minDistance(trow_t table[], int size, bool valid[]) {
 
   vertex = -1;       // error return code
   dist = INFINITY;
-  
 
-  for (i=0; i<size; i++) {
+  for (i = 0; i < size; i++) {
     if (valid[i] && (table[i].distance < dist)) {
       // we found a new min distance
       vertex = i;
       dist = table[i].distance;
-    }    
+    }
   }
 
   return vertex;
@@ -59,8 +58,8 @@ int minDistance(trow_t table[], int size, bool valid[]) {
 void setNeighbors(bool neighbors[], graph_t g, int size, int v) {
   int i;
 
-  for (i=0; i<size; i++) {
-    if (g[v][i]>0) {
+  for (i = 0; i < size; i++) {
+    if (g[v][i] > 0) {
       neighbors[i] = true;
     } else {
       neighbors[i] = false;
@@ -73,7 +72,7 @@ void setNeighbors(bool neighbors[], graph_t g, int size, int v) {
 // remove vertex v from set s
 // note that array s is changed by this function
 void removeVertex(int v, bool s[], int size) {
-  if ((0<=v) && (v<size)) {
+  if ((0 <= v) && (v < size)) {
     s[v] = false;
   } else {
     printf("*** INVALID NODE FOUND while performing set difference\n");
@@ -86,13 +85,13 @@ void removeVertex(int v, bool s[], int size) {
 // print the distance table
 void printTable(trow_t table[], int size, int start) {
   int i;
-  
+
   printf("\nTABLE\n");
 
-  for (i=0; i<size; i++){
+  for (i = 0; i < size; i++){
     printf("[%d-%d]:%d from %d\n", start, i, table[i].distance, table[i].lastStep);
   }
-    
+
 }
 
 // debug print the graph
@@ -101,8 +100,8 @@ void printGraph(graph_t G, int size){
 
   printf("\nGRAPH\n");
 
-  for (i=0; i<size; i++) {
-    for (j=0; j<size; j++) {
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
       printf("|%0.3d", G[i][j]);
     }
     printf("|\n");
@@ -115,28 +114,28 @@ void printSet(bool s[], int size) {
 
   printf("\nSET\n");
 
-  for (i=0; i<size; i++) {
+  for (i = 0; i < size; i++) {
     if (s[i]) {
-      printf("[%d]true ",i);
+      printf("[%d]true ", i);
     } else {
-      printf("[%d]false ",i);
+      printf("[%d]false ", i);
     }
   }
   printf("\n");
 }
 
-  
+
 int main () {
 
   // define graph as association matrix
   graph_t E = {
-    //0  1  2  3  4  5   <- destination vertex
-    { 0, 8,10, 0, 0,15}, // vertex 0 ->
-    { 0, 0,11,16, 0, 0}, // vertex 1 ->
-    { 0, 0, 0,12, 0, 3}, // vertex 2 ->
-    { 0, 0, 0, 0, 0, 0}, // vertex 3 ->
-    { 0, 0, 0, 7, 0, 0}, // vertex 4 ->
-    { 0, 0, 0, 0,10, 0}, // vertex 5 ->
+    //0  1   2   3   4   5   <- destination vertex
+    { 0, 8, 10,  0,  0, 15}, // vertex 0 ->
+    { 0, 0, 11, 16,  0,  0}, // vertex 1 ->
+    { 0, 0,  0, 12,  0,  3}, // vertex 2 ->
+    { 0, 0,  0,  0,  0,  0}, // vertex 3 ->
+    { 0, 0,  0,  7,  0,  0}, // vertex 4 ->
+    { 0, 0,  0,  0, 10,  0}, // vertex 5 ->
   };
 
   // Q is "set" of vertices - true if vertex is in set
@@ -155,49 +154,59 @@ int main () {
 
   // DIJKSTRA: create table T[V] = <infinity, undefined>
   // ********** INSERT YOUR CODE HERE **********
+  for (v = 0; v < GSIZE; v++) {
+    T[v].distance = INFINITY;
+    T[v].lastStep = -1;
+  }
 
   // DIJKSTRA: T[source].distance = 0
   // ********** INSERT YOUR CODE HERE **********
+  T[0].distance = 0;
 
   // DIJKSTRA: Create set Q = set(V)
   // ********** INSERT YOUR CODE HERE **********
-
+  for (v = 0; v < GSIZE; v++) {
+    Q[v] = true;
+  }
 
   // DIJKSTRA: while Q is not empty
-  while (setNotEmpty(Q,GSIZE)) {
+  while (setNotEmpty(Q, GSIZE)) {
     // DIJKSTRA: u = min q in Q of T[q].distance
     // ********** INSERT YOUR CODE HERE **********
+    u = minDistance(T, GSIZE, Q);
 
     // check for errors
-    if (u<0) {
+    if (u < 0) {
       printf("*** INVALID NODE FOUND while finding min distance\n");
       return -1;
     }
 
     // DIJKSTRA: S = neighbors(u)
     // ********** INSERT YOUR CODE HERE **********
-
+    setNeighbors(S, E, GSIZE, u);
 
     // DIJKSTRA: Q = Q – u
     // ********** INSERT YOUR CODE HERE **********
-
+    Q[u] = false;
 
     // DIJKSTRA: for each neighbor v of u (ignore vertices that we have already finished)
-    for (v=0; v<GSIZE; v++) {
+    for (v = 0; v < GSIZE; v++) {
       if (S[v] && Q[v]) {
         // DIJKSTRA: d = T[u].distance + E[u,v]
         // ********** INSERT YOUR CODE HERE **********
-
+        d = T[u].distance + E[u][v];
 
         // DIJKSTRA: if (d < T[v].distance)
-	if (d < T[v].distance) {
-  	  // DIJKSTRA: //shorter path found
-  	  // DIJKSTRA: T[v].distance = d
+        if (d < T[v].distance) {
+          // DIJKSTRA: //shorter path found
+          // DIJKSTRA: T[v].distance = d
           // ********** INSERT YOUR CODE HERE **********
+          T[v].distance = d;
 
-	  // DIJKSTRA: T[v].lastStep = u
+          // DIJKSTRA: T[v].lastStep = u
           // ********** INSERT YOUR CODE HERE **********
-	}
+          T[v].lastStep = u;
+        }
       }
     }
     // DIJKSTRA: //done with u
@@ -205,6 +214,6 @@ int main () {
   }
 
   printTable(T, GSIZE, 0);
-   
+
   return 0;
 }
